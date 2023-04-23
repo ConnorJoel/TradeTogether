1d0cb09d1e2aa1af4f015e11f2ef3b2243e4034a
Connor Joel <cjoel4000@gmail.com>
2023-04-23 14:09:03 -0400

    Initial Commit

---
+++ TradeTogether/TradeTogetherApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradeTogetherApp.swift
+//  TradeTogether
+//
+//  Created by Connor Joel on 4/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TradeTogetherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
